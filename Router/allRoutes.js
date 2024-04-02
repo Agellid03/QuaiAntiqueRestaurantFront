9517c40ac24e93f6133bc5a77c671fa057613aa7
OuldBraham Sofiane <Ouldbrahamsofiane@gmail.com>
2024-04-02 15:03:56 +0200

    Ajout Inscription + Changement des liens navbar "connexion/deconnexion"

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,7 @@ export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "La galerie", "/pages/galerie.html"),
   new Route("/signIn", "Connexion", "/pages/signIn.html"),
+  new Route("/signUp", "Inscription", "/pages/signUp.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
