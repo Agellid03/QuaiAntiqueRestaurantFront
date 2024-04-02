004fe3b34765a70dc42d8b0b228cae0b93dddacb
OuldBraham Sofiane <Ouldbrahamsofiane@gmail.com>
2024-04-02 16:22:17 +0200

    Créa dossier "auth" + créa account.html, editPassword.html

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,8 +4,14 @@ import Route from "./Route.js";
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "La galerie", "/pages/galerie.html"),
-  new Route("/signIn", "Connexion", "/pages/signIn.html"),
-  new Route("/signUp", "Inscription", "/pages/signUp.html"),
+  new Route("/signIn", "Connexion", "/pages/auth/signIn.html"),
+  new Route("/signUp", "Inscription", "/pages/auth/signUp.html"),
+  new Route("/account", "Mon compte", "/pages/auth/account.html"),
+  new Route(
+    "/editPassword",
+    "Changement de mot de passe",
+    "/pages/auth/editPassword.html"
+  ),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
