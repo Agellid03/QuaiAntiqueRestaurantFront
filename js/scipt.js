0e8c61728a3e72704ca06a5728b627a2f440da06
OuldBraham Sofiane <Ouldbrahamsofiane@gmail.com>
2024-04-12 17:14:41 +0200

    Création de cookies et token + fonction de connexion et déconnexion

---
+++ js/scipt.js
@@ -0,0 +1,62 @@
+const tokenCookieName = "accesstoken";
+const btnSignOut = document.getElementById("signOut-btn");
+
+btnSignOut.addEventListener("click", signOut);
+
+function signOut() {
+  eraseCookie(tokenCookieName);
+  window.location.reload();
+}
+
+//*Fonctions set et get Token pour le cookie
+function setToken(token) {
+  setCookie(tokenCookieName, token, 7); // expire dans 1sem
+}
+
+function getToken() {
+  return getCookie(tokenCookieName);
+}
+
+//*Fonctions pré-faites  ( a etudier )
+
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+}
+
+// Fonction pour savoir si l'on est connecté ou pas
+
+function isConnected() {
+  if (getToken() == null || getToken == undefined) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+//*Code execut au chargement de chaque page
+if (isConnected()) {
+  alert("Je suis connecté");
+} else {
+  alert("Je ne suis pas connecté");
+}
