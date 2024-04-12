06be6ca2bd4898a5a561238888e061dce6b0a7d8
OuldBraham Sofiane <Ouldbrahamsofiane@gmail.com>
2024-04-12 10:34:57 +0200

    Créa du dossier js et le fichier signup.js + Ajout verifications du formulaire inscription + création du fichier signin.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,8 +4,18 @@ import Route from "./Route.js";
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "La galerie", "/pages/galerie.html"),
-  new Route("/signIn", "Connexion", "/pages/auth/signIn.html"),
-  new Route("/signUp", "Inscription", "/pages/auth/signUp.html"),
+  new Route(
+    "/signIn",
+    "Connexion",
+    "/pages/auth/signIn.html",
+    "/js/auth/signin.js"
+  ),
+  new Route(
+    "/signUp",
+    "Inscription",
+    "/pages/auth/signUp.html",
+    "/js/auth/signup.js"
+  ),
   new Route("/account", "Mon compte", "/pages/auth/account.html"),
   new Route(
     "/editPassword",
@@ -13,6 +23,7 @@ export const allRoutes = [
     "/pages/auth/editPassword.html"
   ),
   new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
+  new Route("/reserver", "Rserver", "/pages/reservations/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
