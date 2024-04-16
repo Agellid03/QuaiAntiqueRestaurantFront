41a57dac0a9b6327492e0a79e79fa2143b173c8b
OuldBraham Sofiane <Ouldbrahamsofiane@gmail.com>
2024-04-16 18:18:45 +0200

    Ajout de la logique router pour gestion de l'affichage en fonction de l'utilisateur

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,17 @@
 export default class Route {
-  constructor(url, title, pathHtml, pathJS = "") {
+  constructor(url, title, pathHtml, authorize, pathJS = "") {
     this.url = url;
     this.title = title;
     this.pathHtml = pathHtml;
     this.pathJS = pathJS;
+    this.autorize = authorize;
   }
 }
+
+/*
+[] -> Tout le monde peut y accéder
+["admin"] -> Réserver aux utilisateurs avec le role admin 
+["client"] -> Réserver aux utilisateurs avec le role client
+["admin", "client"] -> Réserver aux utilisateurs avec le role client ou admin
+["disconnected"] -> Réserver aux utilisateurs déconnecté 
+*/
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,28 +2,41 @@ import Route from "./Route.js";
 
 //Définir içi les routes
 export const allRoutes = [
-  new Route("/", "Accueil", "/pages/home.html"),
-  new Route("/galerie", "La galerie", "/pages/galerie.html"),
+  new Route("/", "Accueil", "/pages/home.html", []),
+  new Route("/galerie", "La galerie", "/pages/galerie.html", []),
   new Route(
     "/signIn",
     "Connexion",
     "/pages/auth/signIn.html",
+    ["disconnected"],
     "/js/auth/signin.js"
   ),
   new Route(
     "/signUp",
     "Inscription",
     "/pages/auth/signUp.html",
+    ["disconnected"],
     "/js/auth/signup.js"
   ),
-  new Route("/account", "Mon compte", "/pages/auth/account.html"),
+  new Route("/account", "Mon compte", "/pages/auth/account.html", [
+    "client",
+    "admin",
+  ]),
   new Route(
     "/editPassword",
     "Changement de mot de passe",
+    ["client", "admin"],
     "/pages/auth/editPassword.html"
   ),
-  new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
-  new Route("/reserver", "Rserver", "/pages/reservations/reserver.html"),
+  new Route(
+    "/allResa",
+    "Vos réservations",
+    "/pages/reservations/allResa.html",
+    ["client"]
+  ),
+  new Route("/reserver", "Rserver", "/pages/reservations/reserver.html", [
+    "client",
+  ]),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
